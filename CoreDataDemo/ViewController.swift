a7494d82d218b844a4fc117319bab0126fb527be
lokialice <lokialicehd@gmail.com>
2015-05-18 12:49:04 +0700

    CoreDataDemo
    Đây là một trong những lựu chọn tối ưu cho lưu trữ trong ứng dụng, đặc
    biệt là các ứng dụng cỡ trung và lớn. Cung cấp một công cụ trực quan để
    thiết kế cơ sở dữ liệu cùng các quan hệ (Data Relationship), nó còn
    giúp cho developer tiết kiệm đáng kể thời gian viết code. Quan trọng
    hơn Coredata còn giúp giảm đáng kể tài nguyên vùng nhớ (Memory) bởi khả
    năng thu hồi của nó.

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -7,9 +7,52 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var txtUserName: UITextField!
+    @IBOutlet var txtPassword: UITextField!
+    
+    @IBAction func btnSave(sender: UIButton) {
+       println("Save Button Pressed \(txtUserName.text)")
+        var appDel:AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        var context:NSManagedObjectContext = appDel.managedObjectContext!
+        var newUser: AnyObject = NSEntityDescription.insertNewObjectForEntityForName("User", inManagedObjectContext: context)
+        newUser.setValue(txtUserName.text, forKey: "username")
+        newUser.setValue(txtPassword.text, forKey: "password")
+        
+        context.save(nil)
+        
+        println(newUser)
+        println("Object Save")
+        
+    }
+    
+    @IBAction func btnLoad(sender: UIButton) {
+        println("Load Button Pressed \(txtPassword.text)")
+        var appDel:AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        var context:NSManagedObjectContext = appDel.managedObjectContext!
+       var request = NSFetchRequest(entityName: "User")
+        request.returnsObjectsAsFaults = false
+        request.predicate = NSPredicate(format: "username = %@","" + txtUserName.text)
+        
+        var result:NSArray = context.executeFetchRequest(request, error: nil)!
+
+        if(result.count > 0){
+            var res: AnyObject = result[0] as AnyObject
+            txtUserName.text = res.valueForKey("username") as! String
+            txtPassword.text = res.valueForKey("password") as! String
+            
+            
+//            for res in result{
+//                println(res)
+//            }
+        }else {
+            println("0 Result Returned...Potenial error")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
